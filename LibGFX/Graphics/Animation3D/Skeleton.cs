ecb05675da8dafc877a4244e38b2cf0aa9e80964
Andreas Wagner <wagnerandreas17@yahoo.de>
2025-05-08 19:40:01 +0200

    Update

---
+++ LibGFX/Graphics/Animation3D/Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibGFX.Graphics.Animation3D
+{
+    /// <summary>
+    /// Represents a bone information for the rendering pipeline
+    /// </summary>
+    public class Skeleton
+    {
+        /// <summary>
+        /// The Bones of the skeleton.
+        /// </summary>
+        public Dictionary<String, BoneInfo> BoneInfoMap { get; set; }
+
+        /// <summary>
+        /// The number of bones in the skeleton.
+        /// </summary>
+        public int BoneCounter;
+    }
+}
