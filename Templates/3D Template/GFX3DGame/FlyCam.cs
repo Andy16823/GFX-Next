bf4270f8d3bf39e35de0ae0769e2add05b613686
Andreas Wagner <wagnerandreas17@yahoo.de>
2025-05-17 12:44:06 +0200

    Added the 3D Template

---
+++ Templates/3D Template/GFX3DGame/FlyCam.cs
@@ -0,0 +1,113 @@
+﻿using LibGFX;
+using LibGFX.Core;
+using LibGFX.Graphics;
+using LibGFX.Pyhsics;
+using LibGFX.UI;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Keys = OpenTK.Windowing.GraphicsLibraryFramework.Keys;
+
+namespace GFX3DGame
+{
+    public class FlyCam : IGameBehavior
+    {
+        private GameElement _element;
+        private Vector2 _mousePos;
+
+        public GameElement GetElement()
+        {
+            return _element;
+        }
+
+        public void SetElement(GameElement gameElement)
+        {
+            _element = gameElement;
+        }
+
+        public void OnCollide()
+        {
+
+        }
+
+        public void OnDispose(BaseScene scene, IRenderDevice renderer)
+        {
+
+        }
+
+        public void OnInit(BaseScene scene, Viewport viewport, IRenderDevice renderer)
+        {
+
+        }
+
+        public void OnRender(BaseScene scene, Viewport viewport, IRenderDevice renderer, LibGFX.Graphics.Camera camera)
+        {
+
+        }
+
+        public void OnUpdate(BaseScene scene)
+        {
+            var window = GFX.Instance.GetWindow();
+            var camera = (PerspectiveCamera)PerspectiveCamera.Current;
+            var mousePos = window.GetMousePosition();
+
+            var deltaX = _mousePos.X - mousePos.X;
+            var deltaY = _mousePos.Y - mousePos.Y;
+
+            var cameraFront = camera.Transform.GetFront();
+            var cameraRight = camera.Transform.GetRight();
+            var cameraUp = camera.Transform.GetUp();
+
+            var speed = scene.RenderStats.DeltaTime * 0.05f;
+            if (window.IsKeyDown(Keys.A))
+            {
+                camera.Transform.Position -= cameraRight * speed;
+            }
+            else if (window.IsKeyDown(Keys.D))
+            {
+                camera.Transform.Position += cameraRight * speed;
+            }
+            else if (window.IsKeyDown(Keys.W))
+            {
+                camera.Transform.Position += cameraFront * speed;
+            }
+            else if (window.IsKeyDown(Keys.S))
+            {
+                camera.Transform.Position -= cameraFront * speed;
+            }
+            else if (window.IsKeyDown(Keys.PageUp))
+            {
+                camera.Transform.Position += cameraUp * speed;
+            }
+            else if (window.IsKeyDown(Keys.PageDown))
+            {
+                camera.Transform.Position -= cameraUp * speed;
+            }
+
+            if (window.IsMouseDown(MouseButton.Right))
+            {
+                camera.Transform.Rotate(new Vector3(deltaY * 0.1f, deltaX * 0.1f, 0.0f));
+            }
+
+            if (window.IsMouseDown(MouseButton.Left))
+            {
+                var result = Raycast.PerformRaycastFromScreen(camera, window.GetViewport(), (PhysicsHandler3D)scene.PhysicsHandler, (int)mousePos.X, (int)mousePos.Y);
+                if (result.hitElement != null)
+                {
+                    Debug.WriteLine($"Hit Element {result.hitElement.Name}");
+                }
+            }
+            _mousePos = mousePos;
+        }
+
+        public void OnCollide(Collision collision)
+        {
+
+        }
+    }
+}
---
+++ Templates/3D Template/GFX3DGame/Game.cs
@@ -0,0 +1,134 @@
+﻿using LibGFX.Core.GameElements;
+using LibGFX.Core;
+using LibGFX.Graphics.Lights;
+using LibGFX.Graphics.Materials;
+using LibGFX.Graphics;
+using LibGFX.Pyhsics;
+using LibGFX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+using Assimp;
+using System.Diagnostics;
+using LibGFX.Graphics.Enviroment;
+using LibGFX.Graphics.Primitives;
+
+namespace GFX3DGame
+{
+    public class Game
+    {
+        private Window _window;
+        private GLRenderer _renderer;
+        private PerspectiveCamera _camera;
+        private Scene3D _scene;
+        private PhysicsHandler3D _physicsHandler;
+
+        public Game()
+        {
+            // Initialize game components here
+        }
+
+        public void Initialize()
+        {
+            // Create an new game window
+            var viewport = new Viewport(800, 600);
+            _window = GFX.Instance.CreateWindow("GFX", viewport, OpenTK.Windowing.Common.WindowState.Normal);
+
+            // Create an OpenGL renderer
+            _renderer = new GLRenderer();
+            _renderer.Init(_window);
+            _renderer.UseVsync(true);
+
+            // Create the camera for the rendering
+            _camera = new PerspectiveCamera(new Vector3(0.0f, 5f, -7.0f), new Vector3(viewport.Width, viewport.Height, 0.0f));
+            _camera.SetAsCurrent();
+
+            // Create the scene with layers
+            _scene = new Scene3D("BASE_LAYER", "AI_LAYER", "PLAYER_LAYER", "ITEM_LAYER");
+            _scene.Enviroment = new ProceduralSky();
+            _scene.SetDirectionalLight(new DirectionalLight(new Vector3(-0.2f, 1.0f, -0.3f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f), 1.5f));
+            _scene.AddPointLight(new PointLight3D(new Vector3(2f, 0f, 0f), new Vector4(0.8f, 0.0f, 0.0f, 1.0f), 4f, 30f));
+            _scene.AddPointLight(new PointLight3D(new Vector3(-2f, 0f, 0f), new Vector4(0.0f, 0.8f, 0.0f, 1.0f), 4f, 30f));
+            _scene.AddPointLight(new PointLight3D(new Vector3(0f, 2f, 0f), new Vector4(0.0f, 0.0f, 0.8f, 1.0f), 4f, 30f));
+
+            // Load the assets
+            var model = GFX.Instance.AssetManager.Load<Model>("Ressources/Lion/scene.gltf");
+            model.Transform.Position = new Vector3(0.0f, 0.0f, 0.0f);
+            model.Transform.Rotate(0.0f, -90f, 0.0f);
+            model.Transform.Scale = new Vector3(1.5f, 1.5f, 1.5f);
+            model.Shader = _renderer.GetShaderProgram("MeshShader");
+            model.AddBehavior(new FlyCam());
+            _scene.AddGameElement("BASE_LAYER", model);
+            _camera.LookAt(model.Transform.Position);
+
+
+            // Create an 2D physics handler with zero gravity
+            _physicsHandler = new PhysicsHandler3D(Vector3.Zero);
+            _scene.PhysicsHandler = _physicsHandler;
+
+            // Initialize game objects and resources here
+            GFX.Instance.AssetManager.ForeachAsset<IMaterial>(material =>
+            {
+                material.Init(_renderer);
+            });
+
+            // Initialize the scene
+            _scene.Init(_window.GetViewport(), _renderer);
+        }
+
+        public void Run()
+        {
+            // Main game loop
+            while (!_window.RequestClose())
+            {
+                var viewport = _window.GetViewport();
+                _window.ProcessEvents();
+
+                Update();
+                Render(viewport);
+            }
+
+            Dispose();
+        }
+
+        public void Stop()
+        {
+            _window.Close();
+        }
+
+        private void Update()
+        {
+            // Update game logic here
+            _scene.UpdatePhysics();
+            _scene.Update();
+        }
+
+        private void Render(Viewport viewport)
+        {
+            // Clear the screen
+            _renderer.MakeCurrent();
+            _renderer.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            _renderer.Clear(RenderFlags.ClearFlags.Color | RenderFlags.ClearFlags.Depth);
+
+            // Render game graphics here
+            _scene.Render(viewport, _renderer, _camera);
+
+            // Swap buffers
+            _renderer.Flush();
+            _renderer.SwapBuffers();
+
+            // Check for errors
+            Debug.WriteLine($"Render Error {_renderer.GetError()}");
+        }
+
+        private void Dispose()
+        {
+            _scene.DisposeScene(_renderer);
+            _renderer.Dispose();
+        }
+
+    }
+}
