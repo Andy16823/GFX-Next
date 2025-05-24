c2bd949ffcdb4eede9d7169ce96c954692856d53
Andreas Wagner <wagnerandreas17@yahoo.de>
2025-05-24 09:43:30 +0200

    Update

--- LibGFX/Graphics/Viewport.cs
+++ LibGFX/Graphics/Viewport.cs
@@ -7,22 +7,45 @@ using System.Threading.Tasks;
 
 namespace LibGFX.Graphics
 {
+    /// <summary>
+    /// Represents a viewport in 2D or 3D space
+    /// </summary>
     public struct Viewport
     {
+        /// <summary>
+        /// The width of the viewport
+        /// </summary>
         public int Width { get; set; }
+
+        /// <summary>
+        /// The height of the viewport
+        /// </summary>
         public int Height { get; set; }
 
+        /// <summary>
+        /// Creates a new viewport with the given width and height
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
         public Viewport(int width, int height)
         {
             this.Width = width;
             this.Height = height;
         }
 
+        /// <summary>
+        /// Converts the viewport to a Vector2i
+        /// </summary>
+        /// <returns></returns>
         public Vector2i ToVector2I()
         {
             return new Vector2i(Width, Height);
         }
 
+        /// <summary>
+        /// Converts the viewport to a Vector2
+        /// </summary>
+        /// <returns></returns>
         public Vector2 ToVector2()
         {
             return new Vector2(Width, Height);
