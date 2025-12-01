4a02c78dc37480284c45b82de7ede996d3fff716
Andreas Wagner <andy16823@live.de>
2025-12-01 22:05:04 +0100

    Update build for .NET SDK 10.0.100 and NuGet 7.0.0
    Upgraded SdkAnalysisLevel to 10.0.100 and NuGetToolVersion to 7.0.0. Removed unused dependencies from LibGFX.deps.json and updated global usings syntax. This reflects a build with the latest .NET SDK and NuGet tooling.

--- LibGFX/obj/Debug/net8.0/LibGFX.AssemblyInfo.cs
+++ LibGFX/obj/Debug/net8.0/LibGFX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Andy16823")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1-dev+a1c9ec05a470e557882dd0706e3fce64624b26a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1-dev+3b4f1678cc78195e37ddb2bbe7947b91dd0dfa86")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibGFX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibGFX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.1.0")]
