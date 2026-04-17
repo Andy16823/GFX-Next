f6a8f7054ef2948b6a2ff5023d8d66d4e3ac2341
Andreas Wagner <andy16823@live.de>
2026-04-17 06:53:33 +0200

    Update AssemblyInformationalVersion
    Bump AssemblyInformationalVersionAttribute from 1.1.2.3+29a5fe9f3a0b9... to 1.1.2.3+c527d00d256aa... and update AssemblyInfoInputs.cache. Regenerated build outputs (bin/Debug and obj/Debug) including LibGFX DLLs, PDBs and reference assemblies; no other source changes.

--- LibGFX/obj/Debug/net8.0/LibGFX.AssemblyInfo.cs
+++ LibGFX/obj/Debug/net8.0/LibGFX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Andy16823")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.2.3")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.2.3+29a5fe9f3a0b93887d8dc559d5cf5ba1aec8678f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.2.3+c527d00d256aa49fc50d30ad3b3cf45fb97682d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibGFX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibGFX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.2.3")]
